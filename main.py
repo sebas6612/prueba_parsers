2b64cc498822e74b6b1b7f5380ca35d539a22de0
sebas6612 <sebas6612@gmail.com>
2025-05-15 18:22:42 -0500

    main file

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def main():
-    print("Hello from prueba!")
+from src.utils.job_processor import JobProcessor
+
+
+def main() -> None:
+    """
+    Initializes a JobProcessor with the specified job definition file and processes the jobs.
+
+    This function creates an instance of the JobProcessor class using the "job_definition.json" file
+    and calls its process_jobs method to execute the defined jobs.
+    """
+    processor = JobProcessor("job_definition.json")
+    processor.process_jobs()
 
 
 if __name__ == "__main__":
